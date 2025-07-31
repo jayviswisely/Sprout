0df4ff32808275ee747c3746557ff4327bc91fb4
jayviswisely <104494669+jayviswisely@users.noreply.github.com>
2025-07-31 17:51:15 +0800

    implement localStorage for data persistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PortfolioSummary from './components/PortfolioSummary'
 import AssetForm from './components/AssetForm'
 import AssetList from './components/AssetList'
 
 const App = () => {
-  const [portfolio, setPortfolio] = useState([])
+  const [portfolio, setPortfolio] = useState(() => {
+    const savedPortfolio = localStorage.getItem('portfolio')
+    return savedPortfolio ? JSON.parse(savedPortfolio) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('portfolio', JSON.stringify(portfolio))
+  }, [portfolio])
 
   return (
     <>
